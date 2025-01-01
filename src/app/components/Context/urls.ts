cf6a13d16a42d6450dd396505683a390596575d2
vtrankle13 <vtrankle123@gmail.com>
2025-01-01 15:05:09 -0600

    Update urls.ts

--- src/app/components/Context/urls.ts
+++ src/app/components/Context/urls.ts
@@ -1,18 +1,37 @@
 export const urls = [{
-  url: "https://e360.yale.edu/digest/indonesia-malaysia-deforestation",
-  title: "Indonesia Deforestation",
+  url: "https://singita.com/lodge/singita-castleton/",
+  title: "Singita Castelton (Kruger)",
   seeded: false,
   loading: false,
 },
 {
-  url: "https://cleantechnica.com/2023/06/29/solar-82-of-power-capacity-growth-in-india-in-2022/",
-  title: "Solar Power in India",
+  url: "https://www.theroyalportfolio.com/la-residence/",
+  title: "La Residence (Franschhoek)",
   seeded: false,
   loading: false,
 },
 {
-  url: "https://www.cbssports.com/nba/news/matisse-thybulle-to-stay-with-blazers-who-match-mavericks-three-year-33-million-deal-per-report/",
-  title: "Matisee Thybulle",
+  url: "https://www.grandeprovence.co.za/franschhoek-luxury-accommodation/",
+  title: "Grande Provence (Franschhoek)",
   seeded: false,
   loading: false,
-}]
+},
+{
+  url: "https://www.theplettenberghotel.com/",
+  title: "Plettenberg Hotel (Plettenberg Bay)",
+  seeded: false,
+  loading: false,
+},                     
+{
+  url: "https://ricketybridge.com/pages/stay-at-rickety-bridge-manor-house",
+  title: "Rickety Bridge (Franschhoek)",
+  seeded: false,
+  loading: false,
+},
+{
+  url: "https://samsara-africa.com/homepage/concierge-cape-town-samsara/",
+  title: "DMC (SA)",
+  seeded: false,
+  loading: false,
+}                    
+]
